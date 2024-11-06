93a2267730f731c5751f23d339fae9dc780bdb1d
dao <146765250+ssaumdokak@users.noreply.github.com>
2024-11-06 10:45:18 +0100

    Create next.config.js

---
+++ client/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+	reactStrictMode: false,
+	env: {
+		GOOGLE_RECAPTCHA_SITE_KEY: process.env.GOOGLE_RECAPTCHA_SITE_KEY
+	},
+	images: {
+		domains: [
+			'dl-20211030-963.anilib.top',
+			'averlist.ru',
+			'cdn.discordapp.com',
+			'sun2-4.userapi.com',
+			'sun2-11.userapi.com',
+			'localhost',
+			'192.168.1.55'
+		]
+	}
+}
